ad7a0c7e22e2b49c4830d347c920d2fa96835ce9
Joran Bergfeld <joran.bergfeld@devoteam.com>
2020-09-24 14:12:48 +0200

    Adds scripts

---
+++ scripts/installService.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+podman run -d -p 8081:8081 --network=qiot --name=air-quality-management bentaljaard/air-quality-management:aarch64-latest
+cp *.service /etc/systemd/system
+systemctl enable air-quality-management.service
+systemctl start air-quality-management
+#systemctl status air-quality-edge
