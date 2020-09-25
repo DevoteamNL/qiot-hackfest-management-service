f4b34f94d5b11f03edd938ae7b3880dbcd1b8516
Joran Bergfeld <JoranBergfeld@users.noreply.github.com>
2020-09-25 14:18:06 +0200

    Update installService.sh

--- scripts/installService.sh
+++ scripts/installService.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-podman run -d -p 8081:8081 --network=qiot --name=air-quality-management bentaljaard/air-quality-management:aarch64-latest
+podman run -d -p 8081:8081 --network=qiot --privileged --name=air-quality-management bentaljaard/air-quality-management:aarch64-latest
 cp *.service /etc/systemd/system
 systemctl enable air-quality-management.service
 systemctl start air-quality-management
